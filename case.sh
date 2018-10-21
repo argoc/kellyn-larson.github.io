0b7c42a1d152bc44cf584a9f7d669f0b17cd2c93
kellyn-larson <14264073+kellyn-larson@users.noreply.github.com>
2018-10-21 15:45:47 -0600

    Add files via upload

---
+++ case.sh
@@ -0,0 +1,17 @@
+echo
+PROMPT="Hit a key, then hit return: "
+read -p "$PROMPT" key
+
+while [ $key != "X" ] ; do
+	case "$key" in
+  	  [A-Z]   ) echo "Uppercase letter";;
+  	  [a-z]   ) echo "Lowercase letter";;
+  	  [0-9]   ) echo "Digit";;
+  	  ""	  ) echo "Empty String";;
+  	  *       ) echo "Other";;
+	esac
+        read -p "$PROMPT" key
+done     
+
+exit 0 
+
